0d4f4428483b0e22e3a05988d478bbcad04081d8
jarvis2050 <anran.2008.1988@163.com>
2022-07-14 11:22:12 +0800

    add subnet_2

--- main.tf
+++ main.tf
@@ -31,3 +31,13 @@ resource "aws_subnet" "test_aws_subnet" {
     Name = "test_aws_subnet"
   }
 }
+
+resource "aws_subnet" "test_aws_subnet_2" {
+  vpc_id     = aws_vpc.test_aws_vpc.id
+  cidr_block = "10.0.2.0/24"
+#  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "test_aws_subnet_2"
+  }
+}
